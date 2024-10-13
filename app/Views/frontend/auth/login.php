c61e7c083e3f3492d0a5b30b386b61fc809d2eca
Prabhu <vprabhu1771@gmail.com>
2024-10-13 12:50:01 +0530

    login ui added  logic

--- app/Views/frontend/auth/login.php
+++ app/Views/frontend/auth/login.php
@@ -1,12 +1,18 @@
 <?= $this->extend('frontend/layout/app') ?>
 
 <?= $this->section('title') ?>
-   Login Page
+Login Page
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-
-
-<h1>login page</h1>
-
+<div class="login-container">
+    <h1>Welcome To Login Page</h1>
+    <form action="<?= site_url('authenticate') ?>" method="POST">
+        <?= csrf_field() ?>
+        <input type="text" id="email" name="email" placeholder="Email" required>
+        <input type="password" id="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+    <a href="<?= site_url('register') ?>" class="signup-link">Don't have an account? Sign up</a>
+</div>
 <?= $this->endSection() ?>
