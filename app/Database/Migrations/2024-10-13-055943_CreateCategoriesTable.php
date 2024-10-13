e75b1ce38a98b19611a80e7a13cfc9ea4f25d5f6
Prabhu <vprabhu1771@gmail.com>
2024-10-13 11:31:38 +0530

    Create 2024-10-13-055943_CreateCategoriesTable.php
    php spark make:migration create_categories_table

---
+++ app/Database/Migrations/2024-10-13-055943_CreateCategoriesTable.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name'        => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],            
+            'created_at timestamp default current_timestamp',
+            'updated_at timestamp default current_timestamp on update current_timestamp'
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('categories');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('categories');
+    }
+}
